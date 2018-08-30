59ed0f225bb63bdf5c9ffc0c93dfdb6d7ad1bf70
Kiliano Lopes <kiliano.lopes@degraupublicidade.com.br>
2018-08-30 16:34:19 -0300

    Exibição melhor para o usuário

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -21,9 +21,6 @@ turno = 3;
 tabuleiro = new Array(0,0,0,0,0,0,0,0,0);
 // exemplo: tabuleiro[2] = terceiro numero
 
-tabuleiro = new Array(0,0,0,0,3,0,30,0,0);
-
-
 
 // Melhor casa a ser marcada pela ia
 iaIdeia = 100;
@@ -49,21 +46,18 @@ function iaPensar() {
 			if (linhaAnalisada == 6) {
 				if (tabuleiro[iaCasasAnalisadas[0]] == 0) {
 					iaIdeia = iaCasasAnalisadas[0];
-					console.log(iaIdeia);
 					iaLinha = 9;
-					passarTurno(30)
+					passarTurno()
 				} else {
 					if (tabuleiro[iaCasasAnalisadas[1]] == 0) {
 						iaIdeia = iaCasasAnalisadas[1];
-						console.log(iaIdeia);
 						iaLinha = 9;
-						passarTurno(30)
+						passarTurno()
 					} else {
 						if (tabuleiro[iaCasasAnalisadas[2]] == 0) {
 							iaIdeia = iaCasasAnalisadas[2];
-							console.log(iaIdeia);
 							iaLinha = 9;
-							passarTurno(30)
+							passarTurno()
 						}
 					}
 				}
@@ -84,21 +78,18 @@ function iaPensar() {
 			if (linhaAnalisada == 60) {
 				if (tabuleiro[iaCasasAnalisadas[0]] == 0) {
 					iaIdeia = iaCasasAnalisadas[0];
-					console.log(iaIdeia);
 					iaLinha = 9;
-					passarTurno(30)
+					passarTurno()
 				} else {
 					if (tabuleiro[iaCasasAnalisadas[1]] == 0) {
 						iaIdeia = iaCasasAnalisadas[1];
-						console.log(iaIdeia);
 						iaLinha = 9;
-						passarTurno(30)
+						passarTurno()
 					} else {
 						if (tabuleiro[iaCasasAnalisadas[2]] == 0) {
 							iaIdeia = iaCasasAnalisadas[2];
-							console.log(iaIdeia);
 							iaLinha = 9;
-							passarTurno(30)
+							passarTurno()
 						}
 					}
 				}
@@ -109,7 +100,34 @@ function iaPensar() {
 
 	// 3 Se tiver algum movimento que cria duas linhas com duas casas ocupadas, faça ele (criando o empasse contra o inimigo)
 
-		// Esse vai dar trabalho
+	if (turno == 3) {
+		for (iaLinha = 1; iaLinha < 9; iaLinha++ ) {
+			casasAnalisadas();
+			
+			// Analisando
+			var linhaAnalisada = tabuleiro[iaCasasAnalisadas[0]]+tabuleiro[iaCasasAnalisadas[1]]+tabuleiro[iaCasasAnalisadas[2]];
+			if (linhaAnalisada == 60) {
+				if (tabuleiro[iaCasasAnalisadas[0]] == 0) {
+					iaIdeia = iaCasasAnalisadas[0];
+					iaLinha = 9;
+					passarTurno()
+				} else {
+					if (tabuleiro[iaCasasAnalisadas[1]] == 0) {
+						iaIdeia = iaCasasAnalisadas[1];
+						iaLinha = 9;
+						passarTurno()
+					} else {
+						if (tabuleiro[iaCasasAnalisadas[2]] == 0) {
+							iaIdeia = iaCasasAnalisadas[2];
+							iaLinha = 9;
+							passarTurno()
+						}
+					}
+				}
+			}
+
+		}
+	}
 
 
 
@@ -117,17 +135,12 @@ function iaPensar() {
 	if (turno == 3) {
 		if (tabuleiro[4] == 0) {
 			iaIdeia = 4;
-			console.log(iaIdeia);
-			passarTurno(30)
+			passarTurno()
 		}
 	}
 
 	// 5 Se o opotente preencheu uma quina, marque a quina contrária.
 	if (turno == 3) {
-		if (tabuleiro[0] == 30 && tabuleiro[8] == 0) {
-			
-		}
-
 		for (iaLinha = 7; iaLinha < 9; iaLinha++ ) {
 			casasAnalisadas();
 			
@@ -136,15 +149,13 @@ function iaPensar() {
 			if (linhaAnalisada <= 33 && linhaAnalisada > 3) {
 				if (tabuleiro[iaCasasAnalisadas[0]] == 0) {
 					iaIdeia = iaCasasAnalisadas[0];
-					console.log(iaIdeia);
 					iaLinha = 9;
-					passarTurno(30);
+					passarTurno();
 				} else {
 					if (tabuleiro[iaCasasAnalisadas[2]] == 0) {
 						iaIdeia = iaCasasAnalisadas[2];
-						console.log(iaIdeia);
 						iaLinha = 9;
-						passarTurno(30);
+						passarTurno();
 					}
 				}
 			}
@@ -154,7 +165,55 @@ function iaPensar() {
 
 
 	// 6 Se tiver uma quina vazia, preencha.
+	if (turno == 3) {
+		for (iaLinha = 7; iaLinha < 9; iaLinha++ ) {
+			casasAnalisadas();
+			
+			// Analisando
+			var linhaAnalisada = tabuleiro[iaCasasAnalisadas[0]]+tabuleiro[iaCasasAnalisadas[1]]+tabuleiro[iaCasasAnalisadas[2]];
+			if (linhaAnalisada <= 60) {
+				if (tabuleiro[iaCasasAnalisadas[0]] == 0) {
+					iaIdeia = iaCasasAnalisadas[0];
+					iaLinha = 9;
+					passarTurno();
+				} else {
+					if (tabuleiro[iaCasasAnalisadas[2]] == 0) {
+						iaIdeia = iaCasasAnalisadas[2];
+						iaLinha = 9;
+						passarTurno();
+					}
+				}
+			}
+
+		}
+	}
+
+
 	// 7 Marque aleatoriamente um espaço vazio.
+	if (turno == 3) {
+		for (iaLinha = 1; iaLinha < 4; iaLinha++ ) {
+			casasAnalisadas();
+			
+			// Analisando
+			if (tabuleiro[iaCasasAnalisadas[0]] == 0) {
+				iaIdeia = iaCasasAnalisadas[0];
+				iaLinha = 9;
+				passarTurno()
+			} else {
+				if (tabuleiro[iaCasasAnalisadas[1]] == 0) {
+					iaIdeia = iaCasasAnalisadas[1];
+					iaLinha = 9;
+					passarTurno()
+				} else {
+					if (tabuleiro[iaCasasAnalisadas[2]] == 0) {
+						iaIdeia = iaCasasAnalisadas[2];
+						iaLinha = 9;
+						passarTurno()
+					}
+				}
+			}
+		}
+	}
 }
 
 function casasAnalisadas() {
@@ -190,17 +249,65 @@ function casasAnalisadas() {
 }
 
 function zerarTabuleiro() {
+	tabuleiro = new Array(0,0,0,0,0,0,0,0,0);
 
+	// Quem começa
+	var moeda = Math.floor(Math.random() * 2);
+	if (moeda == 0) {
+		turno == 3;
+		console.log("A máquina começa");
+	} else {
+		turno == 30;
+		console.log("O humano começa");
+	}
 }
 
-function passarTurno(jogador) {
-	turno = jogador;
-}
+function passarTurno() {
+	if (turno == 3) {
+		tabuleiro[iaIdeia] = 3;
+		turno = 30;
+		mostrar();
+		console.log("Agora é o turno do Jogador");
+	} else {
+		if (turno == 30) {
+			turno = 3;
+			mostrar();
+			console.log("Agora é o turno da IA");
+		}
 
-function iaMarcar(local) {
-
+	}
+	
 }
 
 function humanoMarcar(local) {
+	tabuleiro[local] = 30;
+	passarTurno();
+}
+
+function mostrar() {
+	tabuleiroMostrar = new Array("","","","","","","","","");
+
+
+	for (iaContar = 0; iaContar < 9; iaContar++ ) {
+		if (tabuleiro[iaContar] == 3) {
+			tabuleiroMostrar[iaContar] = " X";
+		}
+
+		if (tabuleiro[iaContar] == 30) {
+			tabuleiroMostrar[iaContar] = " O";
+		}
+
+		if (tabuleiro[iaContar] == 0) {
+			tabuleiroMostrar[iaContar] = " _";
+		}
+	}
+
+	console.log(tabuleiroMostrar[0]+tabuleiroMostrar[1]+tabuleiroMostrar[2]);
+	console.log(tabuleiroMostrar[3]+tabuleiroMostrar[4]+tabuleiroMostrar[5]);
+	console.log(tabuleiroMostrar[6]+tabuleiroMostrar[7]+tabuleiroMostrar[8]);
 
 }
+
+
+// Começando
+zerarTabuleiro();
