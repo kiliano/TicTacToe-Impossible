d4db86efff995cbab2624e105a80b4121ec835b2
Kiliano Lopes <kiliano.lopes@degraupublicidade.com.br>
2018-08-30 18:07:26 -0300

    Ia básica, porém falha

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -6,11 +6,12 @@
 
 // 1 Checar possibilidade de vitória
 // 2 Se o opotente ocupar 2 casas seguidas, ocupe a terceira
-// 3 Se tiver algum movimento que cria duas linhas com duas casas ocupadas, faça ele (criando o empasse contra o inimigo)
-// 4 Se o espaço do centro estiver vazio, marque lá
-// 5 Se o opotente preencheu uma quina, marque a quina contrária.
-// 6 Se tiver uma quina vazia, preencha.
-// 7 Marque aleatoriamente um espaço vazio.
+// 3 Se o humano puder sinucar
+// 4 se a IA puder sinucar
+// 5 Se o espaço do centro estiver vazio, marque lá
+// 6 Se o opotente preencheu uma quina, marque a quina contrária.
+// 7 Se tiver uma quina vazia, preencha.
+// 8 Marque aleatoriamente um espaço vazio.
 
 
 // Criando variáveis
@@ -20,6 +21,8 @@ turno = 3;
 // 0 = não marcado. 3 ia marcou. 30 humano marcou.
 tabuleiro = new Array(0,0,0,0,0,0,0,0,0);
 // exemplo: tabuleiro[2] = terceiro numero
+
+sinucar = new Array(0,0,0,0,0,0,0,0,0);
 
 
 // Melhor casa a ser marcada pela ia
@@ -47,17 +50,17 @@ function iaPensar() {
 				if (tabuleiro[iaCasasAnalisadas[0]] == 0) {
 					iaIdeia = iaCasasAnalisadas[0];
 					iaLinha = 9;
-					passarTurno()
+					passarTurno();
 				} else {
 					if (tabuleiro[iaCasasAnalisadas[1]] == 0) {
 						iaIdeia = iaCasasAnalisadas[1];
 						iaLinha = 9;
-						passarTurno()
+						passarTurno();
 					} else {
 						if (tabuleiro[iaCasasAnalisadas[2]] == 0) {
 							iaIdeia = iaCasasAnalisadas[2];
 							iaLinha = 9;
-							passarTurno()
+							passarTurno();
 						}
 					}
 				}
@@ -79,17 +82,17 @@ function iaPensar() {
 				if (tabuleiro[iaCasasAnalisadas[0]] == 0) {
 					iaIdeia = iaCasasAnalisadas[0];
 					iaLinha = 9;
-					passarTurno()
+					passarTurno();
 				} else {
 					if (tabuleiro[iaCasasAnalisadas[1]] == 0) {
 						iaIdeia = iaCasasAnalisadas[1];
 						iaLinha = 9;
-						passarTurno()
+						passarTurno();
 					} else {
 						if (tabuleiro[iaCasasAnalisadas[2]] == 0) {
 							iaIdeia = iaCasasAnalisadas[2];
 							iaLinha = 9;
-							passarTurno()
+							passarTurno();
 						}
 					}
 				}
@@ -98,48 +101,91 @@ function iaPensar() {
 		}
 	}
 
-	// 3 Se tiver algum movimento que cria duas linhas com duas casas ocupadas, faça ele (criando o empasse contra o inimigo)
+	// 3 Se o humano puder sinucar
 
 	if (turno == 3) {
+		sinucar = new Array(0,0,0,0,0,0,0,0,0);
+
 		for (iaLinha = 1; iaLinha < 9; iaLinha++ ) {
 			casasAnalisadas();
 			
 			// Analisando
 			var linhaAnalisada = tabuleiro[iaCasasAnalisadas[0]]+tabuleiro[iaCasasAnalisadas[1]]+tabuleiro[iaCasasAnalisadas[2]];
-			if (linhaAnalisada == 60) {
+			if (linhaAnalisada == 30) {
 				if (tabuleiro[iaCasasAnalisadas[0]] == 0) {
-					iaIdeia = iaCasasAnalisadas[0];
-					iaLinha = 9;
-					passarTurno()
-				} else {
-					if (tabuleiro[iaCasasAnalisadas[1]] == 0) {
-						iaIdeia = iaCasasAnalisadas[1];
-						iaLinha = 9;
-						passarTurno()
-					} else {
-						if (tabuleiro[iaCasasAnalisadas[2]] == 0) {
-							iaIdeia = iaCasasAnalisadas[2];
-							iaLinha = 9;
-							passarTurno()
-						}
-					}
+					sinucar[iaCasasAnalisadas[0]] = sinucar[iaCasasAnalisadas[0]]+1;
+				}
+
+				if (tabuleiro[iaCasasAnalisadas[1]] == 0) {
+					sinucar[iaCasasAnalisadas[1]] = sinucar[iaCasasAnalisadas[1]]+1;
+				}
+
+				if (tabuleiro[iaCasasAnalisadas[2]] == 0) {
+					sinucar[iaCasasAnalisadas[2]] = sinucar[iaCasasAnalisadas[2]]+1;
 				}
 			}
+		}
 
+		for (iaCasas = 0; iaCasas < 9; iaCasas++ ) {
+			casasAnalisadas();
+			// Analisando
+			if (sinucar[iaCasas] > 1) {
+				iaIdeia = iaCasas;
+				iaLinha = 9;
+				iaCasas = 9;
+				passarTurno();
+			}
+		}
+	}
+
+	// 4 Se a ia puder sinucar
+
+	if (turno == 3) {
+		sinucar = new Array(0,0,0,0,0,0,0,0,0);
+
+		for (iaLinha = 1; iaLinha < 9; iaLinha++ ) {
+			casasAnalisadas();
+			
+			// Analisando
+			var linhaAnalisada = tabuleiro[iaCasasAnalisadas[0]]+tabuleiro[iaCasasAnalisadas[1]]+tabuleiro[iaCasasAnalisadas[2]];
+			if (linhaAnalisada == 3) {
+				if (tabuleiro[iaCasasAnalisadas[0]] == 0) {
+					sinucar[iaCasasAnalisadas[0]] = sinucar[iaCasasAnalisadas[0]]+1;
+				}
+
+				if (tabuleiro[iaCasasAnalisadas[1]] == 0) {
+					sinucar[iaCasasAnalisadas[1]] = sinucar[iaCasasAnalisadas[1]]+1;
+				}
+
+				if (tabuleiro[iaCasasAnalisadas[2]] == 0) {
+					sinucar[iaCasasAnalisadas[2]] = sinucar[iaCasasAnalisadas[2]]+1;
+				}
+			}
+		}
+
+		for (iaCasas = 0; iaCasas < 9; iaCasas++ ) {
+			casasAnalisadas();
+			// Analisando
+			if (sinucar[iaCasas] > 1) {
+				iaIdeia = iaCasas;
+				iaLinha = 9;
+				iaCasas = 9;
+				passarTurno();
+			}
 		}
 	}
 
 
 
-	// 4 Se o espaço do centro estiver vazio, marque lá
+	// 5 Se o espaço do centro estiver vazio, marque lá
 	if (turno == 3) {
 		if (tabuleiro[4] == 0) {
 			iaIdeia = 4;
-			passarTurno()
+			passarTurno();
 		}
 	}
 
-	// 5 Se o opotente preencheu uma quina, marque a quina contrária.
+	// 6 Se o opotente preencheu uma quina, marque a quina contrária.
 	if (turno == 3) {
 		for (iaLinha = 7; iaLinha < 9; iaLinha++ ) {
 			casasAnalisadas();
@@ -147,12 +193,12 @@ function iaPensar() {
 			// Analisando
 			var linhaAnalisada = tabuleiro[iaCasasAnalisadas[0]]+tabuleiro[iaCasasAnalisadas[1]]+tabuleiro[iaCasasAnalisadas[2]];
 			if (linhaAnalisada <= 33 && linhaAnalisada > 3) {
-				if (tabuleiro[iaCasasAnalisadas[0]] == 0) {
+				if (tabuleiro[iaCasasAnalisadas[0]] == 0 && tabuleiro[iaCasasAnalisadas[1]] == 0) {
 					iaIdeia = iaCasasAnalisadas[0];
 					iaLinha = 9;
 					passarTurno();
 				} else {
-					if (tabuleiro[iaCasasAnalisadas[2]] == 0) {
+					if (tabuleiro[iaCasasAnalisadas[2]] == 0 && tabuleiro[iaCasasAnalisadas[1]] == 0) {
 						iaIdeia = iaCasasAnalisadas[2];
 						iaLinha = 9;
 						passarTurno();
@@ -164,7 +210,7 @@ function iaPensar() {
 	}
 
 
-	// 6 Se tiver uma quina vazia, preencha.
+	// 7 Se tiver uma quina vazia, preencha.
 	if (turno == 3) {
 		for (iaLinha = 7; iaLinha < 9; iaLinha++ ) {
 			casasAnalisadas();
@@ -189,7 +235,7 @@ function iaPensar() {
 	}
 
 
-	// 7 Marque aleatoriamente um espaço vazio.
+	// 8 Marque aleatoriamente um espaço vazio.
 	if (turno == 3) {
 		for (iaLinha = 1; iaLinha < 4; iaLinha++ ) {
 			casasAnalisadas();
@@ -250,14 +296,15 @@ function casasAnalisadas() {
 
 function zerarTabuleiro() {
 	tabuleiro = new Array(0,0,0,0,0,0,0,0,0);
+	sinucar = new Array(0,0,0,0,0,0,0,0,0);
 
 	// Quem começa
 	var moeda = Math.floor(Math.random() * 2);
 	if (moeda == 0) {
-		turno == 3;
+		turno = 3;
 		console.log("A máquina começa");
 	} else {
-		turno == 30;
+		turno = 30;
 		console.log("O humano começa");
 	}
 }
@@ -267,7 +314,7 @@ function passarTurno() {
 		tabuleiro[iaIdeia] = 3;
 		turno = 30;
 		mostrar();
-		console.log("Agora é o turno do Jogador");
+		console.log("IA Marcou "+iaIdeia+"Agora é o turno do Jogador");
 	} else {
 		if (turno == 30) {
 			turno = 3;
